fedbdd8ae9a6d67936cdf2ff66859323450458bc
Chris Rose <offline@offby1.net>
2025-11-11 14:36:15 -0800

    Add a CI workflow that runs tests

--- lacon_v_app/urls.py
+++ lacon_v_app/urls.py
@@ -1,3 +1,6 @@
 app_name = "lacon_v_app"
+from django.urls import path  # noqa: E402
 
-urlpatterns = []
+from . import views  # noqa: E402
+
+urlpatterns = [path("regfox_webhook/", views.regfox_webhook, name="regfox_webhook")]
