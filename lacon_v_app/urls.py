54c3bef16bd32fd630ffe0e6a4aec7605f4e881c
Chris Rose <offline@offby1.net>
2025-11-11 16:05:53 -0800

    Add a stub views for urls to import

--- lacon_v_app/urls.py
+++ lacon_v_app/urls.py
@@ -1,6 +1,5 @@
 app_name = "lacon_v_app"
-from django.urls import path  # noqa: E402
 
-from . import views  # noqa: E402
+from . import views  # noqa: F401,E402
 
-urlpatterns = [path("regfox_webhook/", views.regfox_webhook, name="regfox_webhook")]
+urlpatterns = []
