ed68bfbb90d7fe50e03dc6c88d6c616d0e810d57
Chris Rose <offline@offby1.net>
2025-12-21 09:13:43 -0800

    Add a landing page to LACon that redirects to Auth
    - when the member is not logged in, send to the social login immediately
    - when the member IS logged in, redirect to the nomnom index
    - fix a logout bug where the login form was missing

---
+++ lacon_v_app/views.py
@@ -0,0 +1,30 @@
+from urllib.parse import urlencode
+
+from django.contrib import messages
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, reverse
+
+
+def landing(request: HttpRequest) -> HttpResponse:
+    """LACon landing page view.
+
+    If the member is logged in, redirect to the index.
+
+    If the member is not, redirect immediately to the login page on Authentik"""
+
+    if request.user.is_authenticated:
+        messages.info(
+            request,
+            "You are already logged in. Redirecting to the main page.",
+        )
+        return redirect("index")
+
+    messages.info(
+        request,
+        "You are not logged in. Redirecting to the login page.",
+    )
+    # after login, redirect to the index
+    next_url = reverse("index")
+    login_url = reverse("social:begin", kwargs={"backend": "lacon"})
+    query_params = urlencode({"next": next_url})
+    return redirect(f"{login_url}?{query_params}")
